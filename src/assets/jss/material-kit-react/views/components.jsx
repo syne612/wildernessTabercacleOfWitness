a2693257efe2b05b93c2980f4a44ecd2b13d70ac
icarus <ellishogan95@gmail.com>
2018-12-31 22:48:56 -0500

    all this

--- src/assets/jss/material-kit-react/views/components.jsx
+++ src/assets/jss/material-kit-react/views/components.jsx
@@ -7,15 +7,17 @@ const componentsStyle = {
     textAlign: "left"
   },
   title: {
-    fontSize: "4.2rem",
+    fontSize: "4rem",
     fontWeight: "600",
     display: "inline-block",
     position: "relative"
   },
   subtitle: {
     fontSize: "1.313rem",
-    maxWidth: "500px",
-    margin: "10px 0 0"
+    maxWidth: "650px",
+    margin: "10px 0 0",
+    fontWeight: "500"
+
   },
   main: {
     background: "#FFFFFF",
